1464f7f1245536b2873e0703b1a30e3467a4c172
VitaliySaburdo <vsaburdo@gmail.com>
2022-08-12 22:37:18 +0300

    fix mobile menu

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,32 +1,15 @@
 (() => {
   const menuBtnRef = document.querySelector("[data-menu-button]");
   const mobileMenuRef = document.querySelector("[data-menu]");
-  
-  
+
   menuBtnRef.addEventListener("click", () => {
     const expanded =
       menuBtnRef.getAttribute("aria-expanded") === "true" || false;
 
     menuBtnRef.classList.toggle("is-open");
     menuBtnRef.setAttribute("aria-expanded", !expanded);
-  
+
     mobileMenuRef.classList.toggle("is-open");
-    
   });
 })();
 
-// (() => {
-//   const menuOpenBtn = document.querySelector('[data-menu-open]');
-//   const menuCloseBtn = document.querySelector('[data-menu-close]');
-
-//   const mobileMenu = document.querySelector('[data-menu]');
-//   // const body = document.querySelector('body');
-
-//   menuOpenBtn.addEventListener('click', openModal);
-//   menuCloseBtn.addEventListener('click', openModal);
-
-//   function openModal() {
-//     mobileMenu.classList.toggle('is-open');
-//     // body.classList.toggle('no-scroll');
-//   }
-// })();
